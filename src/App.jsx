9b33a6c58f61f9db3f1e3474dfbacc52a8aba0c0
Sidd <siddhantd37@gmail.com>
2024-05-27 01:59:21 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import Todo from './Todo'
 
 function App() {
-
   return (
-    <>
-
-    </>
+    <div>
+      <Todo/>
+    </div>
   )
 }
 
